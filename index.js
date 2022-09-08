00c554968f4f5e85f7392d25610f2621f93c3f5b
torvalds <18207307296@163.com>
2022-09-08 15:47:59 +0800

    定时任务:中秋节;每日一说:九月变更

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * WechatBot
  *  - https://github.com/gengchen528/wechatBot
  */
-const { WechatyBuilder } = require('wechaty');
+const {WechatyBuilder} = require('wechaty');
 const schedule = require('./schedule/index');
 const config = require('./config/index');
 const untils = require('./utils/index');
@@ -97,12 +97,35 @@ async function onMessage(msg) {
 async function initDay() {
   console.log(`已经设定每日说任务`);
 
+  //设置定时任务
+  config.tasks.forEach(task => {
+
+    schedule.setSchedule(task.date, async () => {
+      console.log('定时任务开始工作啦！');
+      let logMsg;
+      let contact = (await bot.Contact.find({name: task.nick})); // 获取你要发送的联系人
+
+      // 你可以修改下面的 str 来内容和格式
+      // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
+      let str = task.msg;
+      try {
+        logMsg = str;
+        await delay(2000);
+        await contact.say(str); // 发送消息
+      } catch (e) {
+        logMsg = e.message;
+      }
+      console.log(logMsg);
+    })
+  });
+
+  //设定每日说任务
   schedule.setSchedule(config.SENDDATE, async () => {
     console.log('你的贴心小助理开始工作啦！');
     let logMsg;
     let contact =
-      (await bot.Contact.find({ name: config.NICKNAME })) ||
-      (await bot.Contact.find({ alias: config.NAME })); // 获取你要发送的联系人
+        (await bot.Contact.find({name: config.NICKNAME})) ||
+        (await bot.Contact.find({alias: config.NAME})); // 获取你要发送的联系人
     let one = await superagent.getOne(); //获取每日一句
     let weather = await superagent.getTXweather(); //获取天气信息
     let today = await untils.formatDate(new Date()); //获取今天的日期
@@ -111,7 +134,7 @@ async function initDay() {
 
     // 你可以修改下面的 str 来自定义每日说的内容和格式
     // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
-    let str = `${today}\n我们在一起的第${memorialDay}天\n\n入冬了，小邓同学要注意防寒哦！\n\n元气满满的一天开始啦,今天也要开心噢^_^\n\n今日天气\n${weather.weatherTips}\n${weather.todayWeather}\n每日一句:\n${one}\n\n每日土味情话：\n${sweetWord}\n\n————————爱你的小何同学`;
+    let str = `${today}\n我们在一起的第${memorialDay}天\n\n九月小邓同学也要注意防晒哦！\n\n元气满满的一天开始啦,今天也要开心噢^_^\n\n今日天气\n${weather.weatherTips}\n${weather.todayWeather}\n每日一句:\n${one}\n\n每日土味情话：\n${sweetWord}\n\n————————爱你的噗噗同学`;
     try {
       logMsg = str;
       await delay(2000);
@@ -137,6 +160,6 @@ bot.on('logout', onLogout);
 bot.on('message', onMessage);
 
 bot
-  .start()
-  .then(() => console.log('开始登陆微信'))
-  .catch((e) => console.error(e));
+    .start()
+    .then(() => console.log('开始登陆微信'))
+    .catch((e) => console.error(e));
