1edbb6e1d00a2272050109eb20e6dfecda642982
torvalds <18207307296@163.com>
2023-03-23 14:15:08 +0800

    browser重用

--- superagent/amazon_best_sellers.ts
+++ superagent/amazon_best_sellers.ts
@@ -1,11 +1,13 @@
 import {ShopInfo} from "./amazon_shop_info";
-import {Page} from "puppeteer";
+import {Browser, Page} from "puppeteer";
 
 const puppeteer = require('puppeteer');
 const amazon_shop_info = require('./amazon_shop_info');
 
 // 延时函数，防止检测出类似机器人行为操作
 const delay = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+
+let browser: Browser;
 
 export async function getShopInfo(url: string, se: boolean): Promise<ShopInfo[] | undefined[]> {
     return await start(url, se, true);
@@ -41,21 +43,39 @@ async function extractAsinObj(page: Page, url: string): Promise<string[]> {
 
 //设置网址
 async function start(url: string, se: boolean, hl: boolean): Promise<ShopInfo[] | undefined[]> {
+    const setup = async () => {
+        browser = await puppeteer.launch({
+            headless: hl,
+            env: {'--disable-site-isolation-trials': true},
+            args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
+        });
+
+        browser.on('disconnected', setup);
+
+        console.log("Started Puppeteer with pid ${browser.process().pid}");
+    };
+
     //启动浏览器,传入headless为false可以打开窗口
-    const browers = await puppeteer.launch({
-        headless: hl,
-        env: {'--disable-site-isolation-trials': true},
-        args: [
-            '--no-sandbox',
-        ]
-    })
-    //启动新页面
-    const page = await browers.newPage()
+    if (!browser) {
+        browser = await puppeteer.launch({
+            headless: hl,
+            env: {'--disable-site-isolation-trials': true},
+            args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
+        });
+        browser.on('disconnected', setup);
+    }
+    let pages: Page[] = await browser.pages();
+    let page: Page = pages && pages.length > 0 ? pages[0] : await browser.newPage();
+    page.on('error', async err => {
+        console.log('Chrome浏览器页面崩溃：', err);
+        pages = await browser.pages();
+        page = pages && pages.length > 0 ? pages[0] : await browser.newPage();
+    });
     //设置页面打开时的页面宽度高度
     await page.setViewport({
         width: 1920,
         height: 1080,
-    })
+    });
 
     //不设置超时时间
     await page.setDefaultNavigationTimeout(0);
@@ -67,8 +87,6 @@ async function start(url: string, se: boolean, hl: boolean): Promise<ShopInfo[] 
     url = url.replace('pg=1', 'pg=2').replace('zg_bs_pg_1', 'zg_bs_pg_2');
     let asins_page2: any[] = await extractAsinObj(page, url);
 
-    //关闭浏览器
-    await browers.close();
     let asins = [...asins_page1, ...asins_page2];
     //延时十秒钟
     await delay(10000);
