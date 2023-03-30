6865a38a8955a892d35baa22fadde738aa6c578a
torvalds <18207307296@163.com>
2023-03-30 16:33:45 +0800

    asin定时任务开启

--- index.js
+++ index.js
@@ -234,22 +234,22 @@ bot.on('message', onMessage);
 //     })
 //     .catch((e) => console.error(e));
 
-// console.log('你的亚马逊工具定时任务在初始化了！');
+console.log('你的亚马逊工具定时任务在初始化了！');
 
-//设定亚马逊爬虫任务
-// schedule.scheduleJob(config.amazonTask.dateTime, async () => {
-//   console.log('你的亚马逊工具开始工作啦！');
-//
-//   let asinBook = new amazon_shop_info.ShopInfoBook("asin", []);
-//   let asins = config.amazonTask.asins;
-//   for (let i = 0; i < asins.length; i++) {
-//     let result = await amazon_shop_info.getShopInfo(asins[i].asins, false);
-//     asinBook.shopInfoSheetList[i] = new amazon_shop_info.ShopInfoSheet(asins[i].sheetName, result);
-//     await delay(60000);
-//   }
-//   //发送邮件
-//   await amazon_shop_info.sendEmailCompact(asinBook);
-// });
+设定亚马逊爬虫任务
+schedule.scheduleJob(config.amazonTask.dateTime, async () => {
+  console.log('你的亚马逊工具开始工作啦！');
+
+  let asinBook = new amazon_shop_info.ShopInfoBook("asin", []);
+  let asins = config.amazonTask.asins;
+  for (let i = 0; i < asins.length; i++) {
+    let result = await amazon_shop_info.getShopInfo(asins[i].asins, false);
+    asinBook.shopInfoSheetList[i] = new amazon_shop_info.ShopInfoSheet(asins[i].sheetName, result);
+    await delay(30000);
+  }
+  //发送邮件
+  await amazon_shop_info.sendEmailCompact(asinBook);
+});
 
 // schedule.scheduleJob(config.amazonTask.urlDateTime, async () => {
 //   console.log('你的亚马逊工具开始工作啦！');
@@ -273,4 +273,4 @@ bot.on('message', onMessage);
 //   await amazon_shop_info.sendEmailCompact(urlBook);
 // });
 //
-// console.log('你的亚马逊工具定时任务初始化完成！');
+console.log('你的亚马逊工具定时任务初始化完成！');
